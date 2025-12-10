02445dd882b8d1e52d93b9e610c445e84cbdd580
phuc220204 <phuc220204@gmail.com>
2025-12-10 15:47:18 +0700

    ReDeloy

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { HeroUIProvider } from '@heroui/react'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { HeroUIProvider } from "@heroui/react";
+import "./index.css";
+import App from "./App.jsx";
 
 // Vercel Speed Insights - runs on client side only
-if (typeof window !== 'undefined') {
-  import('@vercel/speed-insights/react').then(module => {
-    module.injectSpeedInsights()
-  })
+if (typeof window !== "undefined") {
+  import("@vercel/speed-insights/react").then((module) => {
+    module.default();
+  });
 }
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <HeroUIProvider>
-    <App />
+      <App />
     </HeroUIProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
